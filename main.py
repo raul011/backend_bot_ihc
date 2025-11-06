be010730ec6527f440d8e4364bd887f52d515950
raul <raulalbertoaguilar01@gmail.com>
2025-11-06 07:09:42 -0400

    add welcome /start

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os # Importa os para acceder a las variables de entorno
 app = FastAPI()
 
 # --- Cargar variables de entorno desde .env ---
-load_dotenv()
+#load_dotenv()
 # --- Obtener el token de las variables de entorno ---
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_URL = f"https://api.telegram.org/bot{TOKEN}"
@@ -19,6 +19,17 @@ async def webhook(req: Request):
     if "message" in data:
         chat_id = data["message"]["chat"]["id"]
         text = data["message"].get("text", "")
+
+        if text == "/start":
+            requests.post(f"{TELEGRAM_URL}/sendMessage", json={
+                "chat_id": chat_id,
+                "text": "¡Hola! Bienvenido a DELIVEROO. ¿Qué se te antoja hoy para disfrutar una comida deliciosa?",
+                "reply_markup": {
+                    "keyboard": [["Ver Menú"]],
+                    "resize_keyboard": True
+                }
+            })
+
 
         # Responder al usuario
         requests.post(f"{TELEGRAM_URL}/sendMessage", json={
