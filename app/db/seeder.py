8d676fdfce5f46694744555e7e7fb485763f9997
raul <raulalbertoaguilar01@gmail.com>
2025-11-14 07:25:16 -0400

    add user

--- app/db/seeder.py
+++ app/db/seeder.py
@@ -2,8 +2,6 @@
 from sqlalchemy.orm import Session
 from app.db.session import SessionLocal
 from app.models.user import User
-from app.models.product import Product
-
 from app.core.security import get_password_hash
 
 def seed_users():
@@ -13,6 +11,7 @@ def seed_users():
         initial_users = [
             {"name": "Admin", "email": "admin@example.com", "password": "1234"},
             {"name": "User1", "email": "user1@example.com", "password": "1234"},
+            {"name": "raul_alberto", "email": "raulalberto@gmail.com.com", "password": "1234"},
         ]
 
         for u in initial_users:
@@ -26,25 +25,5 @@ def seed_users():
                 )
                 db.add(user)
         db.commit()
-
-def seed_products(db: Session):
-    productos = [
-        {"name": "Burger 🍔", "price": 4.99, "image": "https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg"},
-        {"name": "Fries 🍟", "price": 1.49, "image": "https://cdn.pixabay.com/photo/2017/01/10/19/05/french-fries-1966528_1280.jpg"},
-        {"name": "Pizza 🍕", "price": 7.99, "image": "https://cdn.pixabay.com/photo/2017/01/22/19/20/pizza-1995434_1280.jpg"},
-    ]
-    for p in productos:
-        exists = db.query(Product).filter_by(name=p["name"]).first()
-        if not exists:
-            db.add(Product(**p))
-    db.commit()
-
-
-def run_seeders():
-    db: Session = SessionLocal()
-    try:
-        seed_users(db)
-        seed_products(db)
-        # aquí puedes agregar más seeders (drivers, orders, etc.)
     finally:
         db.close()
