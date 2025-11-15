5df4343d150275e4e1b18aeb1bc6a00c39fcfca0
raul <raulalbertoaguilar01@gmail.com>
2025-11-15 17:04:51 -0400

    a

--- app/main.py
+++ app/main.py
@@ -4,12 +4,13 @@ from app.db.session import SessionLocal, Base, engine
 
 from app.models import user, order,driver,product
 from app.db.seeder import seed_users
-from app.routers import auth,orders,webhook
+from app.routers import auth,orders,webhook,products
 
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(orders.router)
+app.include_router(products.router)
 app.include_router(webhook.router)
 
 
