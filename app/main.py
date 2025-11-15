4d9b98801185a3d69be042f6f2ecf988b805d0d0
raul <raulalbertoaguilar01@gmail.com>
2025-11-15 17:46:26 -0400

    aaa

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.db.session import SessionLocal, Base, engine
 from app.models import user, order,driver,product
 from app.db.seeder import seed_users
 from app.routers import auth,orders,webhook,products
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
